5197c51b9dc2ed7abdf7b0cf6b8a6bbc9b544c65
clruas <clruas@gmail.com>
2019-12-19 19:22:02 +0000

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = props => {
+    return (
+        <div>Meu React Teste</div>
+    )
+}
+ReactDOM.render(<App />, document.querySelector('#root'))
